7d6c0d57663d3fc0fcc3567960a15187c56f7d77
khiemvo <volebaokhiem1711@gmail.com>
2018-05-31 22:07:37 +0700

    try to getting data from database

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,12 @@
 var express = require('express');
-var model = require('../models');
+var model = require('../../models');
 var router = express.Router();
 
 /* home */
-router.get('/home',function());
+
 /* product */
 /*get all product */
 router.get('/product', function(req, res, next){
-  model.product.findAll({
-    attributes: ['id','id_catalog','id_brand','name', 'image_url', 'desciption', 'price','sold_quantity','remaining_quantity','createdAt','updatedAt']
-  })
-  .then(products => { res.json(products));
 });
 
 /* catalog */
@@ -31,3 +27,5 @@ router.get('/brand', function(req, res, next) {
 
 /* user */
 router.get('/user:1', function(req, res, next){});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ res.json([{
+    id: 1,
+    name: "samsepi0l"
+  }, {
+    id: 2,
+    name: "D0loresH4ze"
+  }]);
 });
 
 module.exports = router;
