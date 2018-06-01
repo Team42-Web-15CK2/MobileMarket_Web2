f9f7c39eb6d83d72e873fcbdf9b3f3c8e5db5826
khiemvo <volebaokhiem1711@gmail.com>
2018-06-01 16:59:06 +0700

    write api needing to use

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api/index');
+var apiRouter = require('./routes/api');
 
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 
