640fe7dbd8c6baa0a1fc3699f8d448203a3e2891
khiemvo278 <volebaokhiem1711@gmail.com>
2018-06-19 11:08:12 +0700

    login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM, {render} from 'react-dom';
 import axios from 'axios';
-import Auth from '../auth/AuthService';
-import { Panel, Form, FormGroup, FormControl, Button, Col, ControlLabel, Checkbox } from 'react-bootstrap';
-
+import {Form, FormGroup, Col, Checkbox, Button, ControlLabel, FormControl} from 'react-bootstrap';
 
 export default class Login extends Component {
 
@@ -11,40 +9,66 @@ export default class Login extends Component {
     super(props);
     this.state = {
       username: null,
-      password: null,
+      password: '',
       token: null,
       message: null,
-      email:null
+      email:''
     };
+    this.onChange = this.onChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
+  
+  onChange (key, value) {
+    this.setState({ [key]: value })
+  }
+
 
   handleFormSubmit(e) {
     e.preventDefault();
-    console.log(this.state);
-    let u = this.state.email;
-    let p =  this.state.password;
     axios.post('/api/user/login', {
-      username: u,
-      password: p
+      username: this.state.email,
+      password: this.state.password
     })
     .then(response => {
       console.log(response); 
-      return false;
     })
   }
 
   render() {
     return (
-      <Form horizontal>
-          <FormGroup controlId="formHorizontalEmail">
-              <Col componentClass={ControlLabel} sm={2}>
-                Email
-              </Col>
-              <Col sm={10}>
-                <FormControl type="email" placeholder="Email" />
-              </Col>
-            </FormGroup>
-      </Form>
+      <div className="login">
+             <Form horizontal>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Email
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="email" placeholder="Email" onChange={e => this.state.email = e.target.value} defaultValue={this.state.email}/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Password
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="password" placeholder="Password" onChange={e => this.state.password = e.target.value} defaultValue={this.state.password}/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Checkbox>Remember me</Checkbox>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Button type="submit" onClick={this.handleFormSubmit}>Sign in</Button>
+                </Col>
+              </FormGroup>
+            </Form>
+      </div>
     )
   }
 
